ff72cfdae4a3b692b8a2b0bffee1bbd53b93a90b
tinydancer96 <mardgelleneri@gmail.com>
2026-05-20 23:53:56 +0100

    completing reverseWord kata

---
+++ reverse_word.js
@@ -0,0 +1,9 @@
+function reverseWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.split("").reverse().join(""))
+    .join(" ");
+}
+
+console.log(reverseWords("This is an example!"));
+console.log(reverseWords("double  spaces"));
